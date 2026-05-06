429fedac2dd75e09d06d67adede1cfa41f36f654
Sandrina sitorus <sandrinasitorus2019@gmail.com>
2026-05-06 22:31:50 +0700

    mengisi model

--- app/Models/pasien.php
+++ app/Models/pasien.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class pasien extends Model
 {
-    //
+    protected $table = 'pasien';
+protected $fillable = [
+'no_rekam_medis',
+'nama_pasien',
+'jenis_kelamin',
+'umur',
+];
 }
